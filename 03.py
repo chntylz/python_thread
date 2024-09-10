ac3f090f830b01ff160fa5996ddc5955d26c50e9
Your Name <you@example.com>
2024-09-10 09:31:57 +0800

    test multi

--- 03.py
+++ 03.py
@@ -6,24 +6,45 @@ import time
 from sys import argv
 import multiprocessing
 
-g_counter = 10
+from HData_eastmoney_day import *
+
+hdata=HData_eastmoney_day("usr","usr")
+
+import datetime as datetime
+
+nowdate=datetime.datetime.now().date()
+nowdate=nowdate-datetime.timedelta(1)
+print("nowdate is %s"%(nowdate.strftime("%Y-%m-%d")))
+
+nowdate_df = hdata.get_data_from_hdata(\
+        start_date=nowdate.strftime("%Y-%m-%d"), \
+        end_date=nowdate.strftime("%Y-%m-%d")\
+        )
+
+
+data_list = np.array(nowdate_df)
+data_list = data_list.tolist()
+
+
+g_counter = 0
 
 def worker(name):
     print("Worker %s started" % (name))
+    return name
 
 
 if __name__ == '__main__':
 
     t1 = time.time()
+    mplist = []
     filename , processes, number = argv
     with multiprocessing.Pool(int(processes)) as pool:
-        pool.map(worker, range(int(number)))
+        #pool.map(worker, range(int(number)))
+        mplist.append(
+            pool.map(worker, data_list))
 
-    '''
-    with multiprocessing.Pool(int(processes)) as pool:
-        for i in range(int(number)):
-            pool.map(worker, str(i))
-    '''
+    print("************************************************************************************")
+    print(mplist)
     t2 = time.time()
     print("t2-t1=%s"%(t2-t1))
     print('g_counter=%d' % (g_counter))
